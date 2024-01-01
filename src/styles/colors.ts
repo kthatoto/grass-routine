63f66ebaa4790adc4d3a2d28513e58eabdaedcb6
kthatoto <kthatoto@gmail.com>
2024-01-02 01:15:36 +0900

    Add notification util

---
+++ src/styles/colors.ts
@@ -0,0 +1,13 @@
+export const COLORS = {
+  black: "#444444",
+
+  white: "#ffffff",
+
+  red: "#f84e59",
+  redPale: "#ffedef",
+
+  green: "#00c182",
+  greenPale: "#e6f9f3",
+
+  saturdayBlue: "#65a7d6",
+};
---
+++ src/utils/notifications.tsx
@@ -0,0 +1,65 @@
+import { faCheckCircle, faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { NotificationsProps, showNotification } from "@mantine/notifications";
+import { COLORS } from "@/styles/colors";
+
+export const showErrorNotification = (
+  message: string,
+  options: Partial<NotificationsProps> = {}
+) => {
+  showNotification({
+    title: message,
+    message: "",
+    icon: (
+      <FontAwesomeIcon
+        icon={faCircleXmark}
+        style={{
+          width: "100%",
+          height: "100%",
+          color: COLORS.red,
+          backgroundColor: COLORS.white,
+          borderRadius: "50%",
+        }}
+      />
+    ),
+    styles: {
+      root: {
+        backgroundColor: COLORS.redPale,
+        borderColor: COLORS.redPale,
+      },
+      title: { color: COLORS.red },
+    },
+    ...options,
+  });
+};
+
+export const showSuccessNotification = (
+  message: string,
+  options: Partial<NotificationsProps> = {}
+) => {
+  showNotification({
+    title: message,
+    message: "",
+    icon: (
+      <FontAwesomeIcon
+        icon={faCheckCircle}
+        style={{
+          width: "100%",
+          height: "100%",
+          color: COLORS.green,
+          backgroundColor: COLORS.white,
+          borderRadius: "50%",
+        }}
+      />
+    ),
+    styles: {
+      root: {
+        backgroundColor: COLORS.greenPale,
+        borderColor: COLORS.greenPale,
+        "&::before": { backgroundColor: COLORS.white },
+      },
+      title: { color: COLORS.green },
+    },
+    ...options,
+  });
+};
