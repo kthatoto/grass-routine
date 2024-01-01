6e34be26db9a5a8972a67977c446ed177f28c12c
kthatoto <kthatoto@gmail.com>
2024-01-02 00:02:11 +0900

    Add app shell

--- src/components/templates/Default.tsx
+++ src/components/templates/Default.tsx
@@ -1,7 +1,6 @@
-import "@mantine/core/styles";
-import "@mantine/notifications/styles";
 import { ReactNode } from "react";
-import { Box, createTheme, MantineProvider, Text } from "@mantine/core";
+import { AppShell, Box, Burger, createTheme, MantineProvider, Text } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { ModalsProvider } from "@mantine/modals";
 import { Notifications } from "@mantine/notifications";
 import authStore from "@/stores/authStore";
@@ -23,20 +22,43 @@ interface TemplateProps {
 }
 
 const Default = ({ children }: TemplateProps) => {
+  const [opened, { toggle }] = useDisclosure();
   const { user, signOut } = authStore();
   return (
     <MantineProvider theme={theme}>
       <Notifications />
       <ModalsProvider>
-        {children}
-        <Box pt={4}>
-          <Text>
-            {"Copyright © "}
-            {"h4topigeon "}
-            {new Date().getFullYear()}
-            {"."}
-          </Text>
-        </Box>
+        <AppShell
+          header={{ height: 60 }}
+          navbar={{
+            width: 300,
+            breakpoint: "sm",
+            collapsed: { mobile: !opened },
+          }}
+          padding="sm"
+        >
+          <AppShell.Header>
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
+            />
+            <div>Logo</div>
+          </AppShell.Header>
+          <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+          <AppShell.Main>
+            {children}
+            <Box pt={4}>
+              <Text>
+                {"Copyright © "}
+                {"h4topigeon "}
+                {new Date().getFullYear()}
+                {"."}
+              </Text>
+            </Box>
+          </AppShell.Main>
+        </AppShell>
       </ModalsProvider>
     </MantineProvider>
   );
